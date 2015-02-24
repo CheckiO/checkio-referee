10d2f341bab65a5d6ae4239be8da5639b8af3976
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-02-24 16:49:22 +0300

    Add comparator for float numbers

---
+++ checkio_referee/comparators.py
@@ -0,0 +1,14 @@
+"""
+This library contains various predefined comparing functions for referee class.
+"""
+
+
+def float_compare(first_float: float, second_float: float, precision: int=3) -> bool:
+    """
+    Compare two float numbers with the given accuracy
+
+    :param first_float, second_float: Numbers to compare
+    :param precision: Quantity of numbers after the dot to compare
+    :return: Are they equal or not
+    """
+    return abs(first_float - second_float) <= 0.1 ** precision
