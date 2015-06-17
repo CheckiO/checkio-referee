6f2808c398d165d06c1973d0cadf8907c999ea06
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-06-17 08:31:39 +0300

    add terms in utils

--- checkio_referee/__init__.py
+++ checkio_referee/__init__.py
@@ -1,2 +1,3 @@
 from checkio_referee.referee import RefereeBase, RefereeCodeGolf, RefereeRank
 from checkio_referee.utils import covercodes, validators, representations
+from checkio_referee.utils.terms import *
